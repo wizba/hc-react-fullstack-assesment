9759738e6d03c85af31da76a39fb071f8031c019
william <williamrabs@gmail.com>
2022-02-19 02:35:49 +0200

    project init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { RecipeComponent } from './recipe/recipe.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [			
+    AppComponent,
+      HeaderComponent,
+      FooterComponent,
+      RecipeComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
